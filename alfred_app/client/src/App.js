c4638db39ca988e52fe2de5a9b91f481c397cf86
PC001 laptop <yago.engels@gmail.com>
2023-04-08 10:23:48 +0200

    iets vergeten lol

--- alfred_app/client/src/App.js
+++ alfred_app/client/src/App.js
@@ -1,5 +1,5 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { createTheme } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { themeSettings } from "theme";
