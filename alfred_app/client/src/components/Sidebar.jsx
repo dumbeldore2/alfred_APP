40c8a39671c457511712bbf735e09c8242dadc9b
PC001 laptop <yago.engels@gmail.com>
2023-04-12 19:48:41 +0200

    side bar deels gemaakt

--- alfred_app/client/src/components/Sidebar.jsx
+++ alfred_app/client/src/components/Sidebar.jsx
@@ -7,6 +7,7 @@ import {
     Iconbutton,
     List,
     ListItem,
+    ListItemButton,
     ListItemIcon,
     ListItemText,
     Typography,
@@ -26,11 +27,36 @@ import {
     TodayOutlined,
     CalendarMonthOutlined,
     AminPanelSettingsOutlined,
-    PieChartOutlined
+    PieChartOutlined,
+    EuroOutlined,
+    QueryStatsOutlined
 } from "@mui/icons-material";
 import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import FlexBetween from './FlexBetween';
+
+const navItems = [
+    {
+        text: "Dashboard",
+        icon: <HomeOutlined/>
+    },
+    {
+        text: "aflred's bank",
+        icon: null,
+    },
+    {
+        text: "Outgoing",
+        icon: <ShoppingCartOutlined/>
+    },
+    {
+        text: "Income",
+        icon: <EuroOutlined/>
+    },
+    {
+        text: "stats",
+        icon: <QueryStatsOutlined/>
+    },
+]
 
 const Sidebar = ({
     drawerWidth,
@@ -72,14 +98,59 @@ const Sidebar = ({
                                     <Typography variant='h4' fontWeight="bold">
                                         ECOMVISION
                                     </Typography>
+                                </Box>
                                     {!isNonMobile && (
                                         <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
                                             <ChevronLeft/>
                                         </IconButton>
                                     )}
-                                </Box>
                             </FlexBetween>
                         </Box>
+                        <List>
+                            {navItems.map(({text,icon}) => {
+                                if(!icon) {
+                                    return (
+                                        <Typography key={text} sx={{m: "2.25rem 0 1rem 3rem"}}>
+                                            {text}
+                                        </Typography>
+                                    )
+                                }
+
+                                const lcText = text.toLowerCase();
+
+                                return(
+                                    <ListItem key={text} disablePadding>
+                                        <ListItemButton
+                                            onClick={() => {
+                                                navigate(`/${lcText}`);
+                                                setActive(lcText);
+                                            }}
+                                            sx = {{
+                                                backgroundColor : active === lcText ? theme.palette.secondary[300] : "transparant",
+                                                color: active === lcText 
+                                                ? theme.palette.primary[600]
+                                                : theme.palette.secondary[100], 
+                                            }}
+                                            >
+                                                <ListItemIcon
+                                                sx={{
+                                                    ml: "2rem",
+                                                    color: active === lcText 
+                                                    ? theme.palette.primary[600]
+                                                    : theme.palette.secondary[200], 
+                                                }}
+                                                >
+                                                    {icon}
+                                                </ListItemIcon>
+                                                <ListItemText primary={text}/>
+                                                {active === lcText && (
+                                                    <ChevronRightOutlined sx={{ml:"auto"}}/>
+                                                )}
+                                            </ListItemButton>
+                                    </ListItem>
+                                );
+                            })}
+                        </List>
                     </Box>
                 </Drawer>
             )}
